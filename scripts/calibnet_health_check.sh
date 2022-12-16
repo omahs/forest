03fa4029605ca00d1812ec2e32675adbc6143a7f
creativcoder <rahul@chainsafe.io>
2022-12-16 12:23:27 +0000

    move calibnet ci checks to a script

---
+++ scripts/calibnet_health_check.sh
@@ -0,0 +1,30 @@
+
+SNAPSHOT_DIRECTORY="/tmp/snapshots"
+
+# Fetch params
+forest-cli fetch-params --keys
+# Download snapshot
+forest-cli --chain calibnet snapshot fetch --aria2 -s $SNAPSHOT_DIRECTORY
+
+# Import snapshot and run Forest
+forest --chain calibnet --target-peer-count 50 --encrypt-keystore false --halt-after-import --height=-200 --import-snapshot $SNAPSHOT_DIRECTORY/*.car
+forest-cli --chain calibnet db stats
+forest --chain calibnet --target-peer-count 50 --encrypt-keystore false --detach
+
+# Validate checkpoint tipset hashes
+forest-cli chain validate-tipset-checkpoints
+
+# wait for sync and check health
+sleep 30m
+forest-cli sync wait
+forest-cli --chain calibnet db stats
+# Export snapshot
+forest-cli snapshot export
+# verify snapshot checksum
+sha256sum -c *.sha256sum
+# Print forest logs
+wget -O metrics.log http://localhost:6116/metrics
+pkill forest
+echo "--- Forest STDOUT ---"; cat forest.out
+echo "--- Forest STDERR ---"; cat forest.err
+echo "--- Forest Prometheus metrics ---"; cat metrics.log
